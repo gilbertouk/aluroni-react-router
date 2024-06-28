78286e3311f0788a663cb4faaa1da81618788d4f
Gilberto Silva <gilbertoantoniouk@gmail.com>
2024-06-28 15:46:04 +0100

    refactor: :recycle: add TagsPrato component

--- src/pages/Prato/index.tsx
+++ src/pages/Prato/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import styles from './Prato.module.scss';
 import cardapio from 'data/cardapio.json';
-import classNames from 'classnames';
+import TagsPrato from 'components/TagsPratos';
 
 export default function Prato() {
   const { id } = useParams();
@@ -24,27 +24,7 @@ export default function Prato() {
         <div className={styles.imagem}>
           <img src={prato.photo} alt={prato.title} />
         </div>
-        <div className={styles.conteudo}>
-          <p className={styles.conteudo__descricao}>{prato.description}</p>
-          <div className={styles.tags}>
-            <div
-              className={classNames({
-                [styles.tags__tipo]: true,
-                [styles[`tags__tipo__${prato.category.label.toLowerCase()}`]]:
-                  true,
-              })}
-            >
-              {prato.category.label}
-            </div>
-            <div className={styles.tags__porcao}>{prato.size}g</div>
-            <div className={styles.tags__qtdpessoas}>
-              Serve {prato.serving} pessoa {prato.serving === 1 ? '' : 's'}
-            </div>
-            <div className={styles.tags__valor}>
-              R$ {prato.price.toFixed(2)}
-            </div>
-          </div>
-        </div>
+        <TagsPrato {...prato} />
       </section>
     </>
   );
