fb073c6c60e918bca450609913e60b137351b2de
Gilberto Silva <gilbertoantoniouk@gmail.com>
2024-06-28 17:31:12 +0100

    feat: :sparkles: add navigate to Cardapio page

--- src/pages/Cardapio/Itens/Item/index.tsx
+++ src/pages/Cardapio/Itens/Item/index.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import styles from './Item.module.scss';
 import { Prato } from 'types/Prato';
 import TagsPrato from 'components/TagsPratos';
+import { useNavigate } from 'react-router-dom';
 
 export default function Item(props: Prato) {
-  const { title, description, photo } = props;
+  const navigate = useNavigate();
+  const { title, description, photo, id } = props;
+
   return (
-    <div className={styles.item}>
+    <div className={styles.item} onClick={() => navigate(`/prato/${id}`)}>
       <div className={styles.item__imagem}>
         <img src={photo} alt={title} />
       </div>
